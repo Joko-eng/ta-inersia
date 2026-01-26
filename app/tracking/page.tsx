dc97ec83c951ae8f64f954de209808594bb6a313
Joko <mohammadjoko1001@gmail.com>
2026-01-26 14:56:30 +0700

    install dnd & header project view

--- app/tracking/page.tsx
+++ app/tracking/page.tsx
@@ -18,11 +18,11 @@ export default function page() {
           <button
             className="
     bg-primary text-white px-6 py-2 rounded-lg text-sm border-2 border-border
-    shadow-[4px_4px_0px_0px_rgba(0,0,0,0.8)]
+    shadow-[2px_2px_0px_0px_rgba(0,0,0,0.8)]
     transition-all duration-150
 
     hover:-translate-x-[1px] hover:-translate-y-[1px]
-    hover:shadow-[5px_5px_0px_0px_rgba(0,0,0,0.8)]
+    hover:shadow-[2px_2px_0px_0px_rgba(0,0,0,0.8)]
 
     active:translate-x-[4px] active:translate-y-[4px]
     active:shadow-none
